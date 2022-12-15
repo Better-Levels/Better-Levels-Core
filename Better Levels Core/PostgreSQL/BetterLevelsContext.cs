7b98aae5cdca098ce67545deb0bfe3855d579359
Alex Gryczewski <agryczewski@outlook.com>
2022-12-14 23:21:42 -0500

    Add Npgsql EF dep

---
+++ Better Levels Core/PostgreSQL/BetterLevelsContext.cs
@@ -0,0 +1,14 @@
+﻿using Better_Levels_Core.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Better_Levels_Core.PostgreSQL
+{
+    public class BetterLevelsContext : DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        public DbSet<Reward> Rewards { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+            => optionsBuilder.UseNpgsql("CONNECTION_STRING");
+    }
+}
